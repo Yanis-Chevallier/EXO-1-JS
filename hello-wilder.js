d3489ce1c53da0e8720959b102a760d434e83f2b
Yanis-Chevallier <yanischevallier2@gmail.com>
2024-12-04 16:09:50 +0100

    Je m'entraine mais je n'y arrive pas trop

---
+++ hello-wilder.js
@@ -0,0 +1,31 @@
+2018
+2+2
+7*7
+
+'Hello'
+"Goodbye"
+'Hello, ' + 'Goodbye'
+
+const familyName = 'Stark'
+const motto = 'Winter is coming'
+let membersCount = 4
+
+const worstMovieEver = 'The Last Jedi'
+const myFavoriteBreakfast = 'Chocolatine'
+
+membersCount = 3
+
+familyName = 'Lannister'
+
+const opinion = 'My favorite House in Game of Thrones is ' + familyName
+membersCount = membersCount - 1
+
+/* hello-wilder.js */
+
+// Define some variables
+const myName = 'Wilder Frey';
+const myCampus = 'The Twins';
+const message = 'Hello ' + myName + '! enjoy studying JavaScript at ' + myCampus + '!';
+
+// Display a popup
+alert(message);
